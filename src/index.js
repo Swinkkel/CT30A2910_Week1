f332dc7bb1dcd5f4a56efd797d8b2ab0083c9c03
Tomi Helin <128401603+Swinkkel@users.noreply.github.com>
2023-06-07 09:40:47 +0000

    Update 11 files

--- src/index.js
+++ src/index.js
@@ -6,5 +6,6 @@ function printHelloWorld() {
 
 document.getElementById("app").innerHTML = `
 <h1>Hello world!</h1>
-<button id="my-button" onclick="printHelloWorld()>Button</button>
+<button id="my-button" onclick="printHelloWorld">Click me</button>
+<h1>test</h1>
 `;
