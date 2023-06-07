8996a0400fb4ebdf3c901f25c80d8c83efde3b07
Tomi Helin <128401603+Swinkkel@users.noreply.github.com>
2023-06-07 09:44:02 +0000

    Update 5 files

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import "./styles.css";
 
 function printHelloWorld() {
   console.log("Hello World");
+  document.getElementById("header_id").textContent("My notebook");
 }
 
 document.getElementById("app").innerHTML = `
-<h1>Hello world!</h1>
+<h1 id="header_id">Hello world!</h1>
 <button id="my-button" onclick="printHelloWorld">Click me</button>
 <h1>test</h1>
 `;
